f94710e03f15d65d8dd21867737be7164bcc524a
Jorge Muñoz Guerrero <131190211+JorgeMunoz1991@users.noreply.github.com>
2023-11-23 15:11:41 +0000

    reparado

--- src/models.py
+++ src/models.py
@@ -5,13 +5,14 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {"id": self.id,
@@ -20,6 +21,7 @@ class User(db.Model):
 
 
 class People(db.Model):
+    __tablename__ = 'people'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
     hair_color = db.Column(db.String(120), unique=True, nullable=False)
@@ -34,6 +36,7 @@ class People(db.Model):
 
 
 class Planet(db.Model):
+    __tablename__ = 'planets'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
 
@@ -44,31 +47,37 @@ class Planet(db.Model):
         return {"id": self.id,
                 "name": self.name}
 
-
-class UserFavoritesPlanets(db.Model):
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=False)
-    
-    def __repr__(self):
-        return '<UserFavoritesPlanets %r>' % self.id
-    
-    def serialize(self):
-        return {"id": self.id,
-                "user_id": self.user_id,
-                "planet_id": self.planet_id}
-
-
-class UserFavoritesPeople(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    people_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=False)
+    people_id= db.Column(db.Integer, db.ForeignKey('people.id'),nullable=True)
+    planets_id= db.Column(db.Integer, db.ForeignKey('planets.id'),nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'),nullable=False)
+    people = db.relationship('People', backref='favorite', lazy=True, cascade='all, delete-orphan')
+    planets = db.relationship('Planet', backref='favorite', lazy=True, cascade='all, delete-orphan')
 
     def __repr__(self):
-        return '<UserFavoritesPeople %r>' % self.id
-    
-    def serialize(self):
-        return {"id": self.id,
-                "user_id": self.user_id,
-                "people_id": self.people_id}
+        return '<Favorite %r>' % self.id
 
+    def serialize(self):
+        people_query= People.query.filter_by(id= self.people_id).first()
+
+        if people_query is None:
+          people_resultado=  None
+        else:
+          people_resultado= people_query.serialize()['name']
+
+        planet_query= Planet.query.filter_by(id= self.planets_id).first()
+
+        if planet_query is None:
+          planet_resultado=  None
+        else:
+          planet_resultado= planet_query.serialize()['name']
+        
+
+        return {
+            "id": self.id,
+            "people": people_resultado,
+            "planets": planet_resultado,
+            "user_id": self.user_id
+        }
